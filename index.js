7a61ad03e49eef23a6c88fb52c00cccf265eadd2
choijake0207 <jake.ch0712@gmail.com>
2024-09-24 15:24:13 -0700

    preload json data

--- index.js
+++ index.js
@@ -4,23 +4,39 @@ const excList = document.querySelector(".exc-list")
 const exc = document.querySelector(".exc")
 const anatomyBox = document.querySelector(".anatomy-container")
 
-
+// Animation Cases
+// 1.) New Select
+// 2.) DeSelect
+// 3.) Switch select => implement load anim
 
 
 
 const targetMuscles = document.querySelectorAll(".target-muscle")
 targetMuscles.forEach(muscle => {
     muscle.addEventListener("click", () => {
+        // clear redundant selection
         if (muscle.classList.contains("selected")) {
             muscle.classList.remove("selected")
-            infoBox.classList.remove("visible").add("hidden")
         } else {
+        // clear previous selection
             document.querySelectorAll(".target-muscle.selected").forEach(selectedMuscle => {
                 selectedMuscle.classList.remove("selected")
             })
+        // create new selection
             muscle.classList.add("selected")
-            infoBox.classList.add("visible").remove("hidden")
-            
+
         }
     })
 })
+
+
+// Preload Data
+let data = []
+window.onload = () => {
+    fetch("./Assets/exercises.json")
+        .then(res => res.json())
+        .then(data => {
+            data = data
+            console.log(data)
+        })
+}
