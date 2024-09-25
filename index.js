d83877ea44a2c7f71f2a37980af270f9a8c62503
choijake0207 <jake.ch0712@gmail.com>
2024-09-25 15:06:14 -0700

    refactor youtube call into async/await try catch block / move inner html render into fetch function / complete loading logic

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ targetMuscles.forEach(muscle => {
         // clear redundant selection
         if (muscle.classList.contains("selected")) {
             muscle.classList.remove("selected")
-            clearExcList()
+            clearCard()
         } else {
         // clear previous selection
             document.querySelectorAll(".target-muscle.selected").forEach(selectedMuscle => {
@@ -42,47 +42,60 @@ targetMuscles.forEach(muscle => {
             const list = jsonList.filter(exc => exc.primaryMuscles.find(primaryMuscle => primaryMuscle === target))
             visibleList = list.slice(0,5)
             console.log(visibleList)
-            displayExcList(target)
+            createCard(target)
 
         }
     })
 })
 
-function displayExcList (target) {
+function createCard (target) {
     excList.innerHTML=""
     infoTitle.innerHTML = ""
     infoTitle.textContent = target.toUpperCase()
     for (let i = 0; i < visibleList.length; i++) {
         let exercise = visibleList[i]
-        const response = fetchYoutube(exercise.name)
-        console.log(response)
         let card = document.createElement("li")
         card.classList.add("exc-card")
         card.classList.add("loading")
-        card.innerHTML = `
-            <h2 class="card-title">${exercise.name}</h2>
-            <h3 class="card-summary">Description</h3>
-            <p class="card-instructions">${exercise.instructions}</p>
-            
-            <p class="card-pLabel">Primary Muscles: ${exercise.primaryMuscles.join(",")}</p>
-            <p class="card-sLabel">Secondary Muscles: ${exercise.secondaryMuscles.length > 0 ? exercise.secondaryMuscles.join(", ") : "--"}</p>
-        `
         excList.appendChild(card)
-
+        fetchYoutubeAndRender(exercise, card)
     }
 }
 
-function clearExcList () {
+function clearCard () {
     excList.innerHTML = ""
     infoTitle.textContent = "Click A Muscle Group To Start"
 }
 
-function fetchYoutube (exc) {
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=1&q=${exc}&key=${key}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-        })
+async function fetchYoutubeAndRender(exercise, card) {
 
+    try {
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=1&q=${exercise}&key=${key}`)
+        const data = await response.json()
+        const video = data.items[0]
+        const thumbnail = video.snippet.thumbnail.default.url
+        card.innerHTML = `
+            <h2 class="card-title">${exercise.name}</h2>
+            <h3 class="card-summary">Description</h3>
+            <p class="card-instructions">${exercise.instructions}</p>
+            <img class="thumbnail" src="${thumbnail}" alt="${video.snippet.title}">
+            <p class="card-pLabel">Primary Muscles: ${exercise.primaryMuscles.join(",")}</p>
+            <p class="card-sLabel">Secondary Muscles: ${exercise.secondaryMuscles.length > 0 ? exercise.secondaryMuscles.join(", ") : "--"}</p>
+        `
+    } catch (error) {
+        console.log(error)
+        setTimeout(() => {
+            card.classList.remove("loading")
+        }, 5000)
+        card.innerHTML = `
+            <h2 class="card-title">${exercise.name}</h2>
+            <h3 class="card-summary">Description</h3>
+            <p class="card-instructions">${exercise.instructions}</p>
+            <img class="error thumbnail">
+            <p class="card-pLabel">Primary Muscles: ${exercise.primaryMuscles.join(",")}</p>
+            <p class="card-sLabel">Secondary Muscles: ${exercise.secondaryMuscles.length > 0 ? exercise.secondaryMuscles.join(", ") : "--"}</p>
+        `
+    }
 }
+
 
