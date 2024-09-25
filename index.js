0ad814daa075671d19feeb4fe8551bdf694802a9
choijake0207 <jake.ch0712@gmail.com>
2024-09-25 14:01:13 -0700

    add json query for selected muscle group // add display function to dynamically create exercise cards

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const infoBox = document.querySelector(".info-container")
 const infoTitle = document.querySelector(".info-title")
 const excList = document.querySelector(".exc-list")
-const exc = document.querySelector(".exc")
 const anatomyBox = document.querySelector(".anatomy-container")
 
 // Animation Cases
 // 1.) New Select
 // 2.) DeSelect
 // 3.) Switch select => implement load anim
+
+// Preload Data
+let data = []
+let visibleList = []
+window.onload = () => {
+    fetch("./Assets/exercises.json")
+        .then(res => res.json())
+        .then(dataJSON => {
+            data = dataJSON
+        })
+}
+
 
 
 
@@ -17,6 +28,7 @@ targetMuscles.forEach(muscle => {
         // clear redundant selection
         if (muscle.classList.contains("selected")) {
             muscle.classList.remove("selected")
+            clearExcList()
         } else {
         // clear previous selection
             document.querySelectorAll(".target-muscle.selected").forEach(selectedMuscle => {
@@ -24,19 +36,43 @@ targetMuscles.forEach(muscle => {
             })
         // create new selection
             muscle.classList.add("selected")
-
+            const selection = muscle.getAttribute("id")
+            const [gender, target] = selection.split("-")
+            const list = data.filter(exc => exc.primaryMuscles.find(primaryMuscle => primaryMuscle === target))
+            visibleList = list.slice(0,5)
+            console.log(visibleList)
+            displayExcList(target, list)
         }
     })
 })
 
-
-// Preload Data
-let data = []
-window.onload = () => {
-    fetch("./Assets/exercises.json")
-        .then(res => res.json())
-        .then(data => {
-            data = data
-            console.log(data)
-        })
+function displayExcList (target) {
+    excList.innerHTML=""
+    infoTitle.innerHTML = ""
+    infoTitle.textContent = target.toUpperCase()
+    for (let i = 0; i < visibleList.length; i++) {
+        let exercise = visibleList[i]
+        let card = document.createElement("li")
+        card.classList.add("exc-card")
+        let cardTitle = document.createElement("h2")
+        cardTitle.textContent = exercise.name
+        let pMuscle = document.createElement("p")
+        pMuscle.textContent = `Primary Muscles: ${exercise.primaryMuscles[0]}`
+        let sMuscle = document.createElement("p")
+        sMuscle.textContent = `Seconday Muscles: ${exercise.secondaryMuscles.length > 0 ? exercise.secondaryMuscles[0] : "--"}`
+        card.append(cardTitle, pMuscle, sMuscle)
+        excList.appendChild(card)
+    }
 }
+
+function clearExcList () {
+    excList.innerHTML = ""
+    infoTitle.textContent = "Click A Muscle Group To Start"
+}
+
+function fetchYoutube () {
+    const response = Youtube.Search
+}
+
+
+const youtubeSearchURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=${maxResults}&q=${encodeURIComponent(query)}&key=${API_KEY}`;
